a7659d7bd67ab98a0839756e78a194e0be97fb04
Michael Medina <michaeljmedina22800@gmail.com>
2024-07-09 20:50:11 -0400

    moved arrays to libs folder

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import inquirer from 'inquirer';
 import * as fs from 'node:fs';
 
 // Import questions array
-import questions from './questions.js';
+import questions from './libs/questions.js';
 
 // Import licenses array
-import licenses from './licenses.js';
+import licenses from './libs/licenses.js';
 
 // Callback function to write README file with user inputs
 const generateREADME = (answers) => {
